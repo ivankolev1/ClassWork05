0153693ad2639948c660b8e2415e75062758944c
ivankolev1 <ivankolev1996@gmail.com>
2016-10-05 21:41:31 +0300

    Add files via upload

---
+++ src/Break.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Break {
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int sum = 0;
+		while(number !=0)
+		{
+			sum += number;
+			
+			System.out.println(sum);
+			 number = sc.nextInt();
+			
+		
+		}System.out.println(sum);
+	}			
+
+
+}
