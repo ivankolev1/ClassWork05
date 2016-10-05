993405ba75a9b5f24d12c89dd867043d0772137f
ivankolev1 <ivankolev1996@gmail.com>
2016-10-05 21:46:52 +0300

    Add files via upload

---
+++ src/NoResidue.java
@@ -0,0 +1,15 @@
+
+public class NoResidue {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=100;i++)
+		{
+			if(i%7==0 || i%11==0)
+			{
+				System.out.print(i + " ");
+			}
+		}
+	}
+
+}
